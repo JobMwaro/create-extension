8c53d25bf4f46d7461b59a9b3369cb8827c5d33f
JobMwaro <jobmwaro@gmail.com>
2023-12-26 23:20:25 +0300

    feat: edits step description🏆

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -2,7 +2,7 @@
 function getSrl(){
   chrome.storage.local.get(null, function(result) {
     var allKeys = Object.keys(result);
-    
+
     allKeys.sort(function(a, b) {
       return parseInt(a.slice(4)) - parseInt(b.slice(4));
     });
@@ -16,40 +16,64 @@ function getSrl(){
       var stepFooter = document.createElement('div');
       var stepImg = document.createElement('img');
       var stepTitle = document.createElement("h3");
-      var stepDescription = document.createElement("p");
+      var stepDescription = document.createElement("span");
       stepContainer.setAttribute("class", "stepContainer");
       stepContainerAfter.setAttribute("class", "stepContainerAfter");
       stepHeader.setAttribute("class", "stepHeader");
       stepFooter.setAttribute("class", "stepFooter");
-      var stepContainerId = 'stepContainer'+key;
-      var stepHeaderId = 'stepHeader'+key;
-      var stepFooterId = 'stepFooter'+key;
-      stepContainer.id = stepContainerId;
-      stepHeader.id = stepHeaderId;
-      stepFooter.id = stepFooterId;
+      stepDescription.setAttribute("class", "stepDescription");
+      var stepContainerIdSetter = 'stepContainer'+key;
+      var stepHeaderIdSetter = 'stepHeader'+key;
+      var stepFooterIdSetter = 'stepFooter'+key;
+      var stepDescriptionIdSetter = 'stepDescription'+key;
+      stepContainer.id = stepContainerIdSetter;
+      stepHeader.id = stepHeaderIdSetter;
+      stepFooter.id = stepFooterIdSetter;
+      stepDescription.id = stepDescriptionIdSetter;
       document.body.appendChild(stepContainer);
       document.body.appendChild(stepContainerAfter);
-      var stepContainerClass = document.querySelector('#'+stepContainerId);
-      stepContainerClass.appendChild(stepHeader);
-      stepContainerClass.appendChild(stepFooter);
-      var stepHeaderClass = document.querySelector('#'+stepHeaderId);
-      var stepFooterClass = document.querySelector('#'+stepFooterId);
+      var stepContainerIdGetter = document.querySelector('#'+stepContainerIdSetter);
+      stepContainerIdGetter.appendChild(stepHeader);
+      stepContainerIdGetter.appendChild(stepFooter);
+      var stepHeaderIdGetter = document.querySelector('#'+stepHeaderIdSetter);
+      var stepFooterIdGetter = document.querySelector('#'+stepFooterIdSetter);
       stepImg.id = key;
       stepImg.src = value;
       stepImg.style.width = '120%';
-      stepHeaderClass.appendChild(stepImg);
-      // var stepImgId = document.querySelector('#'+key);
-      stepFooterClass.appendChild(stepTitle);
-      stepFooterClass.appendChild(stepDescription);
+      stepHeaderIdGetter.appendChild(stepImg);
+      stepFooterIdGetter.appendChild(stepTitle);
+      stepFooterIdGetter.appendChild(stepDescription);
       stepTitle.innerHTML = 'Step #'+stepNo;
       stepDescription.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempor id turpis in porttitor. Vivamus ex felis, efficitur in sodales sit amet, dapibus efficitur ante. Aliquam eu pretium nibh, ut elementum purus. Sed dignissim dui a varius pretium. In sit amet eleifend dui, non consequat ante.";
-      
     }
 
+    let parent = document.body;
+    parent.addEventListener("dblclick", function(event) {
+      let target = event.target;
+      if (target.classList.contains("stepDescription")) {
+        let input = document.createElement("input");
+        input.value = target.textContent;
+        input.type = "text";
+        input.className = "stepDescriptionEdit";
+        target.replaceWith(input);
+        input.focus();
+        parent.addEventListener("click", function(event) {
+          if (event.target == input) {
+            input.focus();
+          }
+          else {
+            let value = input.value;
+            let newText = document.createElement("span");
+            newText.textContent = value;
+            newText.id = "text";
+            newText.className = "stepDescription";
+            input.replaceWith(newText);
+          }
+        });
+      }
+    });
     
   });
-
-  
 }
 
 getSrl();
