1527fc65306dd70106a26f0984661c8fd396fbfc
JobMwaro <jobmwaro@gmail.com>
2024-01-09 17:08:31 +0300

    feat: adds user title [extension page & pdf] | auto-adjusts image positioning | validates max-limit ✌️🤸‍♂️

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -1,5 +1,19 @@
 
 function getSrl(){
+    var userGuideTitle = document.createElement('div');
+    var userGuideTitleContent = document.createElement('span');
+    userGuideTitle.setAttribute('class', 'userGuideTitle');
+    userGuideTitleContent.setAttribute('class', 'userGuideTitleContent');
+    userGuideTitle.id = 'userGuideTitle';
+    userGuideTitleContent.id = 'userGuideTitleContent';
+    document.body.appendChild(userGuideTitle);
+    userGuideTitleContent.innerHTML = 'New user Step by step guide Title, [double click to edit]';
+    var userGuideTitleGetter = document.querySelector('#userGuideTitle');
+    userGuideTitleGetter.appendChild(userGuideTitleContent);
+    let stepContainerAfter = document.createElement('div');
+    stepContainerAfter.setAttribute("class", "stepContainerAfter");
+    document.body.appendChild(stepContainerAfter);
+
   chrome.storage.local.get(null, function(result) {
     var allKeys = Object.keys(result);
 
@@ -44,20 +58,27 @@ function getSrl(){
       stepFooterIdGetter.appendChild(stepTitle);
       stepFooterIdGetter.appendChild(stepDescription);
       stepTitle.innerHTML = 'Step #'+stepNo;
-      stepDescription.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempor id turpis in porttitor. Vivamus ex felis, efficitur in sodales sit amet, dapibus efficitur ante. Aliquam eu pretium nibh, ut elementum purus. Sed dignissim dui a varius pretium. In sit amet eleifend dui, non consequat ante.";
+      stepDescription.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempor id turpis in porttitor. Vivamus ex felis, efficitur in sodales sit amet, dapibus efficitur ante. Aliquam eu pretium nibh.";
     }
 
     let parent = document.body;
     parent.addEventListener("dblclick", function(event) {
       let target = event.target;
       if (target.classList.contains("stepDescription")) {
-        let input = document.createElement("input");
+        let input = document.createElement("textarea");
         input.value = target.textContent;
         input.id = target.id;
-        input.type = "text";
         input.className = "stepDescriptionEdit";
         target.replaceWith(input);
         input.focus();
+        input.cols = 30; // Number of columns
+        input.rows = 5;  // Number of rows
+        input.addEventListener('input', function() {
+          const maxLength =215;
+          if (input.value.length > maxLength) {
+            input.value = input.value.slice(0, maxLength);
+          }
+        });
         parent.addEventListener("click", function(event) {
           if (event.target == input) {
             input.focus();
@@ -68,6 +89,35 @@ function getSrl(){
             newText.textContent = value;
             newText.id = input.id;
             newText.className = "stepDescription";
+            input.replaceWith(newText);
+          }
+        });
+      }
+      else if (target.className.includes("userGuideTitleContent")) {
+        let input = document.createElement("textarea");
+        input.value = target.textContent;
+        input.id = target.id;
+        input.className = "stepDescriptionEdit";
+        target.replaceWith(input);
+        input.focus();
+        input.cols = 30; // Number of columns
+        input.rows = 5;  // Number of rows
+        input.addEventListener('input', function() {
+          const maxLength = 65;
+          if (input.value.length > maxLength) {
+            input.value = input.value.slice(0, maxLength);
+          }
+        });
+        parent.addEventListener("click", function(event) {
+          if (event.target == input) {
+            input.focus();
+          }
+          else {
+            let value = input.value;
+            let newText = document.createElement("span");
+            newText.textContent = value;
+            newText.id = input.id;
+            newText.className = "userGuideTitleContent";
             input.replaceWith(newText);
           }
         });
